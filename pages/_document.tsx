29c7c4ec1252746169a268e8ed7379edcff57061
Haruki Izumihara <izu.0622h@gmail.com>
2022-05-01 16:03:39 +0900

    added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,41 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body className="font-sans antialiased">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
